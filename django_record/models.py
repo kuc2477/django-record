88d29500822b4be16e51515849eaf1367cf2a47e
June <kuc2477@gmail.com>
2015-05-13 15:34:06 +0900

    renamed recording_* -> monitored_*

--- django_record/models.py
+++ django_record/models.py
@@ -11,56 +11,56 @@ from django.db.models import DateTimeField
 
 
 class TimeStampedModel(Model):
-    created_time = DateTimeField(auto_now=True)
-    modified_time = DateTimeField(auto_now_add=True)
+    created = DateTimeField(auto_now=True)
+    modified = DateTimeField(auto_now_add=True)
 
     class Meta:
         abstract = True
-        ordering = ('created_time',)
-        get_latest_by = 'created_time'
+        ordering = ('created',)
+        get_latest_by = 'created'
 
 
 class RecordModelMetaClass(ModelBase):
     def __init__(cls, name, bases, attrs):
-        # Ensure that record field registration is done only on
+        # Ensure that monitored field registration is done only on
         # subclasses of RecordModel (not RecordModel itself).
         if name == 'RecordModel':
             return
 
-        recording_model = attrs.get('recording_model')
-        recording_fields = attrs.get('recording_fields')
+        monitored_model = attrs.get('recording_model')
+        monitored_fields = attrs.get('recording_fields')
 
         # Only subclasses of Django Models are recordable.
-        assert(issubclass(recording_model, Model))
+        assert(issubclass(monitored_model, Model))
 
-        # Register recording fields to the subclass of a RecordModel and keep
-        # their names for later usage.
+        # Register monitored and recorded fields to the subclass of a
+        # RecordModel and keep their names for later usage.
         field_names = []
-        for field_entry in recording_fields:
-            # recording field given in a tuple format (properties allowed).
+        for field_entry in monitored_fields:
+            # monitored field given in a tuple format (properties allowed).
             if isinstance(field_entry, tuple):
                 field_name, field = field_entry
 
                 assert(isinstance(field, Field))
                 field.contribute_to_class(cls, field_name)
 
-            # recording field given in ordinary format (ordinary fields).
+            # monitored field given in ordinary format (ordinary fields).
             else:
                 field_name = field_entry
 
-                field = deepcopy(recording_model._meta.get_field(field_name))
+                field = deepcopy(monitored_model._meta.get_field(field_name))
                 field.contribute_to_class(cls, field.name)
 
             field_names.append(field_name)
 
         # Register foreign key.
-        ForeignKey(recording_model, related_name='records')\
-            .contribute_to_class
+        foreign_key = ForeignKey(monitored_model, related_name='records')
+        foreign_key.contribute_to_class(cls, )
 
         def recorder(sender, **kwargs):
             # Ensure that only models that are explicitly told to be
             # are recorded.
-            if not sender == recording_model:
+            if not sender == monitored_model:
                 return
 
             # NOT IMPLEMENTED YET
@@ -71,7 +71,7 @@ class RecordModelMetaClass(ModelBase):
 class RecordModel(TimeStampedModel):
     __metaclass__ = RecordModelMetaClass
 
-    recording_model = NotImplemented
+    monitored_model = NotImplemented
 
     # List of model's fields or properties to be recorded.
     #
@@ -79,11 +79,11 @@ class RecordModel(TimeStampedModel):
     # model field to record the model's property rather than ordinary model
     # field.
     #
-    # If you are just recording ordinary django model field, then it's
+    # If you want to monitor on ordinary django model field, then it's
     # ok to just simply put the field's name instead of a tuple.
     #
     # Example: [('full_name', CharField(max_length=100)), 'created_time', ...]
-    recording_fields = NotImplemented
+    monitored_fields = NotImplemented
 
     class Meta(TimeStampedModel.Meta):
         abstract = True
